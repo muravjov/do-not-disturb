86ab9074d03ddb63f000ec311eb38f9ef2594f44
Ilya Murav'jov <muravev@yandex.ru>
2018-10-11 09:25:16 +0300

    GPLv2+ license to be compatible with https://extensions.gnome.org

--- extension.js
+++ extension.js
@@ -1,3 +1,22 @@
+//
+// This file is part of Do Not Disturb Time, GNOME Shell extension.
+// Copyright (C) 2018 Ilya Murav'jov <muravev@yandex.ru>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
 /* St = Shell Toolkit */
 const St = imports.gi.St;
 const PanelMenu = imports.ui.panelMenu;
